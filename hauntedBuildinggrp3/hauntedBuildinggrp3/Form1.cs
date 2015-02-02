8c2b5b0fd84e369656f5238e527cd548e99ad866
Justo <jdiaze2@uic.edu>
2015-02-02 11:49:38 -0600

    added C# code

---
+++ hauntedBuildinggrp3/hauntedBuildinggrp3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hauntedBuildinggrp3
+{
+    public partial class Form1 : Form
+    {
+        HauntedBuilding hb;
+        public Form1()
+        {
+            InitializeComponent();
+            hb = new HauntedBuilding();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = hb.getTitle();
+        }
+    }
+ 
+}
---
+++ hauntedBuildinggrp3/hauntedBuildinggrp3/HauntedBuilding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hauntedBuildinggrp3
+{
+    class HauntedBuilding
+    {
+        private String title;
+        public HauntedBuilding(){
+            title = "Welcome to Haunted Building\n";
+        }
+
+        public String getTitle(){
+            return title;
+        }
+
+    }
+}
