e982d70d126fdaca4d669bba2e22fc49071f8746
Justo <jdiaze2@uic.edu>
2015-02-04 20:54:55 -0600

    First version, got FORWARD command implemented

--- hauntedBuildinggrp3/hauntedBuildinggrp3/Form1.cs
+++ hauntedBuildinggrp3/hauntedBuildinggrp3/Form1.cs
@@ -12,32 +12,74 @@ namespace hauntedBuildinggrp3
 {
     public partial class Form1 : Form
     {
-        HauntedBuilding hb;
+        private Game.HauntedBuilding hb;
+        private Game.Graphic currentGraphic = new Game.Graphic("");
         private int state = 0;
+        private bool helping = true; //used in Help click event
+
+        //Constructor
         public Form1()
         {
             InitializeComponent();
-            hb = new HauntedBuilding();
+            hb = new Game.HauntedBuilding();
         }
 
+        //ignore this
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             ;
         }
 
+        //Start button
         private void button1_Click(object sender, EventArgs e)
         {
-            if(state == 0)
+            if(state == 0) //first time they click "Start"
             {
-                textBox1.Text = hb.getTitle();
-                textBox1.AppendText("Press start to begin.");
+                textBox1.Text = hb.getTitle() + System.Environment.NewLine +
+                                "Press start to begin.";
+
+                currentGraphic.setGraphic(textBox1.Text);
+
+                state = 1;
+            }
+
+            //TODO startGame will need parameters like loading a saved file 
+            //call startGame() 
+            //If they keep clicking "Start" restart the game
+            else
+            {
+               Game.Graphic graphic = hb.startGame();
+               textBox1.Text = graphic.getGraphic();
+
+               currentGraphic.setGraphic(textBox1.Text);
+            }
+                
+        }
+
+        //Help button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (helping)
+            {
+                Game.Graphic graphic = hb.getHelp();
+                textBox1.Text = graphic.getGraphic();
+                helping = false;
             }
             else
             {
-
-                hb.startGame();
+                textBox1.Text = currentGraphic.getGraphic();
+                helping = true;
             }
-                
+
+        }
+
+        //TODO Enter button & error check
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Game.Graphic graphic = hb.enterCommand(textBox2.Text);
+            textBox1.Text = graphic.getGraphic();
+
+            currentGraphic.setGraphic(textBox1.Text);
         }
     }
  
--- hauntedBuildinggrp3/hauntedBuildinggrp3/HauntedBuilding.cs
+++ hauntedBuildinggrp3/hauntedBuildinggrp3/HauntedBuilding.cs
@@ -4,15 +4,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hauntedBuildinggrp3
+namespace Game
 {
+    class Graphic //simulate graphics (for now just text)
+    {
+        private String graphic;
+
+        public Graphic(String graphic)
+        {
+            this.graphic = graphic;
+        }
+
+        public String getGraphic()
+        {
+            return graphic;
+        }
+
+        public void setGraphic(String graphic)
+        {
+            this.graphic = graphic;
+        }
+    }
+    
+    
+    // FORWARD decrements X
+    // BACKWARD increments x
+    // LEFT decrements Y
+    // RIGHT increments Y
+
+    // Coordinate (9,4) is marked as [x]. We use (x,y) format.
+    //Johnny starts on the 10th floor at coordinate (9,4)
+
+    /*   0        Y         9
+     * 0 [][][][][][][][][][]
+     *   [][][][][][][][][][]
+     *   [][][][][][][][][][]
+     *   [][][][][][][][][][] 
+     * X [][][][][][][][][][]
+     *   [][][][][][][][][][] 
+     *   [][][][][][][][][][] 
+     *   [][][][][][][][][][]
+     *   [][][][][][][][][][]
+     * 9 [][][][][x][][][][][]
+     * */
+
+    //TODO need constructor that initializes floor with a 10x10 matrix of tiles
     class Floor{
 
     }
 
+    //TODO
+    class Tile
+    {
+
+    }
+
+    //TODO
+    class Item
+    {
+
+    }
+
+
+    //just helper class to pass coordinates around easier.
+    class Coordinate
+    {
+        public int x;
+        public int y;
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     class Player{
-        private Floor floor;
-        private String name;
+        private Floor floor; //what floor are they on
+        private int xPos; //Coordinates
+        private int yPos;
+        private Item inventory; // should be an ArrayList
+        private String name; //their name
 
         public void setFloor(Floor floor){
             this.floor = floor;
@@ -23,6 +94,44 @@ namespace hauntedBuildinggrp3
             this.name = name;
         }
 
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public void setCoord(Coordinate coord)
+        {
+            this.xPos = coord.x;
+            this.yPos = coord.y;
+        }
+
+        public bool move(int where)
+        {
+            switch (where)
+            {
+                    //TODO write an enum
+                //FORWARD
+                case 0: if(xPos - 1 < 0) return false; //they hit boundary 
+                        xPos--; break;
+                //RIGHT
+                case 1: if (yPos + 1 > 9 ) return false;
+                        yPos++; break;
+                //BACKWARD
+                case 2: if (xPos + 1 < 9) return false;
+                        xPos++; break;
+                //LEFT
+                case 3: if (yPos - 1 < 0) return false;
+                        yPos--; break;
+                default:
+                    return false; //bad direction
+                
+            }
+
+            return true; //success!
+        }
+
+        public Coordinate getCoordinates() { return new Coordinate(xPos, yPos); }
+
     }
 
     class HauntedBuilding
@@ -30,28 +139,68 @@ namespace hauntedBuildinggrp3
         private String title;
         private Floor[] floors;
         private Player player;
+        private Graphic currentGraphic; //the current image saved.
 
         public HauntedBuilding(){
             title = "Welcome to Haunted Building\n";
-            floors = new Floor[10];
+            floors = new Floor[10]; //Creating 10 foors
             player = new Player();
+            currentGraphic = new Graphic(""); //empty image on screen
         }
 
         public String getTitle(){
             return title;
         }
 
-        public void startGame()
+        public Graphic startGame()
         {
-            player.setFloor(floors[9]);
-            player.setName("Johnny");
+            player.setFloor(floors[9]); //start at the roof
+            player.setName("Johnny"); //for now we call him Johnny
+            player.setCoord(new Coordinate(9,4)); //Floor matrix is zero indexed!! so 10th tile is 9
 
+            Graphic graphic = new Graphic(player.getName() + " is on the roof. (10th floor)");
 
+            currentGraphic = graphic;
+            return graphic;
         }
 
         public Player getPlayer()
         {
             return player;
+        }
+
+        //TODO error check the command
+        public Graphic enterCommand(String command)
+        {
+            Graphic graphic = new Graphic("Bad command! Click Help for help.");
+
+            if (command == "FORWARD")
+            {
+                if (!player.move(0))
+                    graphic.setGraphic("There is wall in front of you!" + System.Environment.NewLine);
+                else{
+                    Coordinate coord = player.getCoordinates();
+                    graphic.setGraphic(player.getName() + " moved to (" + coord.x + "," + coord.y + ")" + System.Environment.NewLine);
+                }
+                }
+
+            return graphic;
+        }
+
+        public Graphic getHelp()
+        {
+            String howto = "Enter commands in the textbox and click 'Enter'" + System.Environment.NewLine +
+                            "The following are supported commands" + System.Environment.NewLine;
+            String move = "LEFT: Move left" + System.Environment.NewLine +
+                            "RIGHT: Move right" + System.Environment.NewLine +
+                            "FORWARD: Move forward" + System.Environment.NewLine +
+                            "BACKWARD: Move backward" + System.Environment.NewLine;
+            String enter = "ENTER: Enter the elevator" + System.Environment.NewLine;
+            String pickup = "PICKUP: Pickup an item" + System.Environment.NewLine;
+            String invt = "INVT: Display items currently held by the player" + System.Environment.NewLine;
+            String backTogame = "Click 'Help' to go back to game screen" + System.Environment.NewLine;
+            
+            return new Graphic(howto + move + enter + pickup + invt + backTogame);
         }
 
     }
